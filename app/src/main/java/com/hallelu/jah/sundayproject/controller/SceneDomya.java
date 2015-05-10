885d8e99e4676e9f7ee093063255e21a14868137
domya <kimuranian@gmail.com>
2015-05-10 21:01:06 +0900

    Facebook,twitter,LINEの投稿機能（簡易）。しっかりした連携はしてない。

--- app/src/main/java/com/hallelu/jah/sundayproject/controller/SceneDomya.java
+++ app/src/main/java/com/hallelu/jah/sundayproject/controller/SceneDomya.java
@@ -1,5 +1,9 @@
 package com.hallelu.jah.sundayproject.controller;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,11 +25,67 @@ public class SceneDomya extends SceneBase {
 
         View rootView = getRootView();
 
+        Button facebookButton = (Button)rootView.findViewById(R.id.button_facebook);
+        facebookButton.setOnClickListener(new FacebookClickLister());
+        Button twitterButton = (Button)rootView.findViewById(R.id.button_twitter);
+        twitterButton.setOnClickListener(new TwitterClickLister());
+        Button lineButton = (Button)rootView.findViewById(R.id.button_line);
+        lineButton.setOnClickListener(new LineClickLister());
+
         Button backButton = (Button)rootView.findViewById(R.id.button_back);
-        backButton.setOnClickListener(new backClickListener());
+        backButton.setOnClickListener(new BackClickListener());
     }
 
-    class backClickListener implements View.OnClickListener {
+    class  FacebookClickLister implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if(isShareAppInstall("com.facebook.katana")){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setPackage("com.facebook.katana");
+                intent.setType("text/plain");
+                intent.putExtra( Intent.EXTRA_TEXT, "http://www.yahoo.co.jp/");
+                getActivity().startActivity(intent);
+            }else{
+                shareAppDl("com.facebook.katana");
+            }
+        }
+    }
+    class  TwitterClickLister implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            String str = "domyaaa";
+            String url = "http://twitter.com/share?text=" + str;
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            getActivity().startActivity(intent);
+        }
+    }
+    class  LineClickLister implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (!isShareAppInstall("jp.naver.line.android")){
+                try {
+                    getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=jp.naver.line.android"))); // Google Play の Facebook アプリインストールページヘ
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=jp.naver.line.android")));
+                }
+                return;
+            }
+
+            String lineComment = "domya!!";
+            try {
+                String lineString = "line://msg/text/" + lineComment;
+                Intent intent = Intent.parseUri(lineString, Intent.URI_INTENT_SCHEME);
+                getActivity().startActivity(intent);
+            } catch (Exception e) {
+                //LINE投稿失敗
+                Log.e("domya","line error001");
+            }
+
+        }
+    }
+
+    class BackClickListener implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
@@ -33,4 +93,22 @@ public class SceneDomya extends SceneBase {
         }
     }
 
+
+    // アプリがインストールされているかチェック
+    private Boolean isShareAppInstall(String packageName){
+        try {
+            PackageManager pm = getActivity().getPackageManager();
+            pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    // アプリが無かったのでGooglePalyに飛ばす
+    private void shareAppDl(String packageName){
+        Uri uri = Uri.parse("market://details?id="+packageName);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        getActivity().startActivity(intent);
+    }
 }
